a184752a25cc2cd4186e34c26c3aa81243be7b6b
Yara Shehadeh <yarafshehadeh@gmail.com>
2023-10-22 18:46:13 +0300

    committed map page until it's fixed

--- lib/src/ui/screens/map_page.dart
+++ lib/src/ui/screens/map_page.dart
@@ -9,26 +9,27 @@ class MapPage extends StatefulWidget{
   _MapPage createState()=> _MapPage();
 }
 class _MapPage extends State<MapPage>{
-  Completer<GoogleMapController> _controller = Completer();
+  // Completer<GoogleMapController> _controller = Completer();
+  //
+  // static const LatLng _center = const LatLng(45.521563, -122.677433);
 
-  static const LatLng _center = const LatLng(45.521563, -122.677433);
-
-  void _onMapCreated(GoogleMapController controller) {
-    _controller.complete(controller);
-  }
+  // void _onMapCreated(GoogleMapController controller) {
+  //   _controller.complete(controller);
+  // }
     @override
     Widget build(BuildContext context){
       return Scaffold(
         appBar: AppBar(
           title: Text("Home page",style: TextStyle(color: Colors.black),),
         ),
-        body:GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom:11.0,
-          ),
-        ),
+        //body:
+        // GoogleMap(
+        //   onMapCreated: _onMapCreated,
+        //   initialCameraPosition: CameraPosition(
+        //     target: _center,
+        //     zoom:11.0,
+        //   ),
+        // ),
       );
     }
   }
