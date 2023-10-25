2663d59e5f29517d481bda13683a284dad9c2119
Yara Shehadeh <yarafshehadeh@gmail.com>
2023-10-25 13:28:21 +0300

    created the Drawer (Side Menu) Need to modify the icons and export them from figma Need to Add routing to pages particularly the sign in/sign up pages

---
+++ lib/src/ui/screens/side_menu.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:pharmaease/src/ui/screens/onboarding_screen.dart';
+
+class SideMenu extends StatelessWidget {
+  const SideMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+        color: const Color(0xFF199A8E),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            SizedBox(
+              height: 120,
+              child: DrawerHeader(
+                  padding: const EdgeInsetsDirectional.only(top: 30, start: 13),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'PharmaEase',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineMedium
+                            ?.copyWith(
+                                fontWeight: FontWeight.w700,
+                                color: Colors.white,
+                                fontSize: 30),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Flexible(
+                          child: Text(
+                        "Helping you every step of the way",
+                        style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.white70,
+                            fontWeight: FontWeight.w400),
+                      )),
+                    ],
+                  )),
+            ),
+            const Divider(
+              color: Colors.white,
+            ),
+            const ListTile(
+              leading: Icon(Icons.home, size: 28,color:Colors.white70),
+              minLeadingWidth: 3,
+              title: Text(
+                'Home',
+                style: TextStyle(color: Colors.white, fontSize: 20,fontWeight: FontWeight.w400),
+              ),
+            ),
+            const ListTile(
+              leading: Icon(Icons.search, size: 28,color:Colors.white70),
+              minLeadingWidth: 3,
+              title: Text(
+                'Search Drug',
+                style: TextStyle(color: Colors.white, fontSize: 20,fontWeight: FontWeight.w400),
+              ),
+            ),
+            const ListTile(
+              leading: Icon(Icons.message_outlined, size: 28,color:Colors.white70),
+              minLeadingWidth: 3,
+              title: Text(
+                'ChatBot',
+                style: TextStyle(color: Colors.white, fontSize: 20,fontWeight: FontWeight.w400),
+              ),
+            ),
+            const ListTile(
+              leading: Icon(Icons.message_outlined, size: 28,color:Colors.white70),
+              minLeadingWidth: 3,
+              title: Text(
+                'Request Restock',
+                style: TextStyle(color: Colors.white, fontSize: 20,fontWeight: FontWeight.w400),
+              ),
+            ),
+            const ListTile(
+              leading: Icon(Icons.person, size: 28,color:Colors.white70),
+              minLeadingWidth: 3,
+              title: Text(
+                'Sign in',
+                style: TextStyle(color: Colors.white, fontSize: 20,fontWeight: FontWeight.w400),
+              ),
+             // ADD ROUTING TO SIGN IN PAGE
+             // onTap: Navigator.pushReplacement(context, OnBoardingScreen()),
+            ),
+            const SizedBox(
+              height: 290,
+            ),
+            const ListTile(
+              leading: Icon(
+                Icons.help_outline,
+                size: 28,
+                  color:Colors.white70
+              ),
+              minLeadingWidth: 3,
+              title: Text(
+                'Help',
+                style: TextStyle(color: Colors.white, fontSize: 20,fontWeight: FontWeight.w400),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
