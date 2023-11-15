cac26fda7ccc620adedb50a239d445dc4aad3db7
Yara Shehadeh <yarafshehadeh@gmail.com>
2023-11-15 17:36:53 +0300

    added medicine details screen page

---
+++ lib/src/ui/screens/medicine_details_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MedicineDetailsScreen extends StatelessWidget{
+
+  MedicineDetailsScreen({Key? key}):super(key:key);
+
+  @override
+  Widget build(BuildContext context){
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
