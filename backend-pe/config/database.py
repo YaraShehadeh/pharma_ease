ae6da161c95e09e0953f2467c1205c34a2cd43b9
yaznzamel <yaznzamel2@gmail.com>
2023-11-16 22:44:33 +0300

    modularizing the code web-service-data-model layers

--- backend-pe/config/database.py
+++ backend-pe/config/database.py
@@ -5,9 +5,6 @@
 
 # db = client.pharmacy_db
 # collection_name = db["pharmacy"]
-
-
-
 import motor.motor_asyncio
 
 uri = "mongodb+srv://admin:admin@cluster0.7p0u15r.mongodb.net/?retryWrites=true&w=majority"
@@ -17,10 +14,10 @@ db = client.pharmacy_db
 collection_name = db["pharmacy"]
 users_collection = db["users"]
 
+
 async def setup_indexes():
     # Create unique indexes
     await collection_name.create_index("name", unique=True)
     await collection_name.create_index("email", unique=True)
     await collection_name.create_index("location", unique=True)
-    await collection_name.create_index("pharmacists.username", unique=True)
-    
+    await collection_name.create_index("pharmacists.username", unique=True)    
--- backend-pe/routes/pharmacy.py
+++ backend-pe/routes/pharmacy.py
@@ -6,8 +6,8 @@ from config.database import collection_name
 from schema.pharmacy import pharmacyEntity, pharmaciesEntity
 from bson import ObjectId
 from models.mlocation import Location
-from geopy import distance
 from typing import List, Optional
+from services.pharmacy_operations import *
 
 pharmacy = APIRouter()
 
@@ -16,61 +16,25 @@ pharmacy = APIRouter()
 @pharmacy.get("/all")
 async def get_all_pharmacies():
     pharmacies = await collection_name.find().to_list(1000)
-    print(pharmaciesEntity(pharmacies))
-    return pharmaciesEntity(pharmacies)
+    return get_all_service(pharmacies)
 
 
+# Fetch all pharmacies that have the specified drug
 @pharmacy.get("/search_drug")
 async def search_drug(drug_name: str, user_lat: float, user_lon: float):
-    # Fetch all pharmacies that have the specified drug
-    pharmacies = await collection_name.find({"drugs.name": drug_name}).to_list(1000)
-
-    if not pharmacies:
-        raise HTTPException(status_code=404, detail="No pharmacies found with the specified drug")
-
-    # Calculate distance from user location to each pharmacy and sort
-    for pharmacy in pharmacies:
-        pharmacy_location = (pharmacy["location"]["latitude"], pharmacy["location"]["longitude"])
-        user_loc = (user_lat, user_lon)
-        pharmacy["distance"] = distance.distance(pharmacy_location, user_loc).km
-
-    # Sort pharmacies by distance
-    sorted_pharmacies = sorted(pharmacies, key=lambda x: x["distance"])[:5]
-
-    return pharmaciesEntity(sorted_pharmacies)  # Pass sorted_pharmacies to the entity function
+    return await search_for_drug_service(drug_name, user_lat, user_lon) 
 
 
 # add a drug to the pharmacy
 @pharmacy.post("/{pharmacy_name}/add_drug")
 async def add_drug_to_pharmacy(pharmacy_name: str, drug: Drug):
-    # Fetch the pharmacy document by its name
-    pharmacy = await collection_name.find_one({"name": pharmacy_name})
-    if not pharmacy:
-        raise HTTPException(status_code=404, detail="Pharmacy not found")
-
-    # Append the new drug
-    if "drugs" not in pharmacy:
-        pharmacy["drugs"] = [drug.dict()]
-    else:
-        pharmacy["drugs"].append(drug.dict())
-
-    # Update the pharmacy document
-    await collection_name.update_one(
-        {"name": pharmacy_name},
-        {"$set": {"drugs": pharmacy["drugs"]}}
-    )
-
-    return {"message": "Drug added successfully"}
+    return await add_drug_service(pharmacy_name, drug)
 
 
 # create a pharmacy in mdb
 @pharmacy.post("/create")
 async def add_pharmacy(pharmacy: Pharmacy):
-    pharmacy_dict = pharmacy.dict()
-    pharmacy_id = await collection_name.insert_one(pharmacy_dict)
-    return {"id": str(pharmacy_id.inserted_id)}
-
-
+    return await add_pharmacy_service(pharmacy)
 
 
 # get pharmacy by id
@@ -91,7 +55,7 @@ async def get_pharmacy_by_name(pharmacy_name: str):
         return pharmacyEntity(pharmacy)
     
     else:
-        raise HTTPException(status_code= 404 , detail ="Pharmacy not found")
+        raise HTTPException(status_code=404, detail="Pharmacy not found")
 
 
 
@@ -105,7 +69,8 @@ async def delete_pharmacy(pharmacy_id: str):
 @pharmacy.put("/{pharmacy_id}")
 async def update_pharmacy(pharmacy_id: str, updated_pharmacy: Pharmacy):
     pharmacy_dict = updated_pharmacy.dict()
-    result = await collection_name.replace_one({"_id": ObjectId(pharmacy_id)}, pharmacy_dict)
+    result = await collection_name.replace_one(
+        {"_id": ObjectId(pharmacy_id)}, pharmacy_dict)
     if result.matched_count == 0:
         raise HTTPException(status_code=404, detail="Pharmacy not found")
     return {"status": "success", "message": "Pharmacy updated successfully"}
