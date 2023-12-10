51e772c6471aa8b27c9369a512130ddd64ea2b69
yaznzamel <yaznzamel2@gmail.com>
2023-12-10 20:07:28 +0300

    changes the vars names

--- backend-pe/models/mpharmacy.py
+++ backend-pe/models/mpharmacy.py
@@ -5,9 +5,15 @@ from models.mlocation import Location
 from models.mpharmacist import Pharmacist
 
 class Pharmacy(BaseModel):
-    name: str = Field(..., description= "Please add the pharmacy name", unique = True)
-    email: str = Field(..., description= "Please add the pharamcy email" , unique = True)
-    description: str = Field(..., description= "Please add the pharamcy description")
+    pharmacyName: str = Field(..., description= "Please add the pharmacy name", unique = True)
+    pharmacyemail: str = Field(..., description= "Please add the pharamcy email" , unique = True)
+    pharmacyDescription: str = Field(..., description= "Please add the pharamcy description")
+    pharmacyImage: str = Field(..., description= "Please add the pharamcy image")
+    pharmacyArea: str = Field(..., description= "Please add the pharamcy area")
+    pharmacyDistance: str = Field(..., description= "Please add the pharamcy distance")
+    pharmacyOpeningHours: str = Field(..., description= "Please add the pharamcy opening hours")
+    pharmacyClosingHours: str = Field(..., description= "Please add the pharamcy closing hours")
+    pharmacyPhoneNumber: str = Field(..., description= "Please add the pharamcy phone number")
     location: Location = Field(..., description= "Please add the pharamcy location", unique = True)
     drugs: List[Drug] = Field(..., description="Please add drugs to the pharmacy")
     pharmacists: List[Pharmacist] = Field(..., description="please enter pharmacist details")
