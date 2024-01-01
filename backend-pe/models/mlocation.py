3339726a1a6ea7b8b72583c1880d267fb4a03bec
yaznzamel <yaznzamel2@gmail.com>
2024-01-01 20:52:02 +0300

    changed pharmacies radius to only accept jordan

--- backend-pe/models/mlocation.py
+++ backend-pe/models/mlocation.py
@@ -1,6 +1,20 @@
-
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, validator
 
 class Location(BaseModel):
     longitude: str
     latitude: str
+
+    @validator('longitude')
+    def validate_longitude(cls, value):
+        value = float(value)
+        if not (35 <= value <= 37):
+            raise ValueError('The longitude must be between 35 and 37')
+        return value
+
+    @validator('latitude')
+    def validate_latitude(cls, value):
+        value = float(value)
+        if not (29 <= value <= 33):
+            raise ValueError('The latitude must be between 29 and 33')
+        return value
+
